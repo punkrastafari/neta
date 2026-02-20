fd3d0354f529d4613193bead10b9638231dab843
punkrastafari <XANDEWERNECK_@HOTMAIL.COM>
2026-02-20 07:20:12 -0300

    Update ama.py

--- ama.py
+++ ama.py
@@ -1,48 +1,30 @@
-# 1. Limpa e instala a nova versão
-!pip install --no-cache-dir git+https://github.com/guiworldtv2/allfinder.git
+import subprocess
 
-# 2. Gera a lista com TODOS os links
-!allfinder \
-"https://www.ms.now/live" \
-"https://globoplay.globo.com/v/4613774/" \
-"https://globoplay.globo.com/ao-vivo/7689934/" \
-"https://globoplay.globo.com/ao-vivo/7690141/" \
-"https://globoplay.globo.com/ao-vivo/7813173/" \
-"https://g1.globo.com/go/goias/ao-vivo/policia-civil-realiza-coletiva-de-imprensa-sobre-o-caso-da-corretora-de-imoveis-encontrada-morta.ghtml" \
-"https://g1.globo.com/sp/campinas-regiao/ao-vivo/eptv-1-campinas-ao-vivo.ghtml" \
-"https://globoplay.globo.com/ao-vivo/14164032" \
-"https://globoplay.globo.com/ao-vivo/2134039/" \
-"https://globoplay.globo.com/v/12749215/" \
-"https://g1.globo.com/rr/roraima/video/ao-vivo-assista-o-jornal-de-roraima-1a-edicao-2923545-1739458038240.ghtml" \
-"https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/bom-dia-cidade-ribeirao-preto.ghtml" \
-"https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/eptv1.ghtml" \
-"https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/eptv-2-ribeirao-e-franca-ao-vivo.ghtml" \
-"https://g1.globo.com/pe/petrolina-regiao/ao-vivo/ao-vivo-assista-ao-gr2.ghtml" \
-"https://g1.globo.com/ap/ao-vivo/assista-ao-bdap-desta-sexta-feira-7.ghtml" \
-"https://globoplay.globo.com/v/1328766/" \
-"https://globoplay.globo.com/v/1467373/" \
-"https://globoplay.globo.com/v/4064559/" \
-"https://globoplay.globo.com/v/5472979/" \
-"https://globoplay.globo.com/v/2135579/" \
-"https://globoplay.globo.com/v/6120663/" \
-"https://globoplay.globo.com/v/2145544/" \
-"https://globoplay.globo.com/ao-vivo/10865071/" \
-"https://globoplay.globo.com/v/4039160/" \
-"https://globoplay.globo.com/v/6329086/" \
-"https://globoplay.globo.com/v/11999480/" \
-"https://g1.globo.com/al/alagoas/ao-vivo/assista-aos-telejornais-da-tv-gazeta-de-alagoas.ghtml" \
-"https://globoplay.globo.com/ao-vivo/3667427/" \
-"https://globoplay.globo.com/v/4218681/" \
-"https://globoplay.globo.com/v/12945385/" \
-"https://globoplay.globo.com/v/3065772/" \
-"https://globoplay.globo.com/v/2923579/" \
-"https://g1.globo.com/am/amazonas/ao-vivo/assista-aos-telejornais-da-rede-amazonica.ghtml" \
-"https://globoplay.globo.com/v/2923546/" \
-"https://globoplay.globo.com/v/2168377/" \
-"https://globoplay.globo.com/v/992055/" \
-"https://globoplay.globo.com/v/602497/" \
-"https://globoplay.globo.com/v/8713568/" \
-"https://globoplay.globo.com/v/10747444/" \
-"https://globoplay.globo.com/v/10740500/" \
-"https://abcnews.com/live/video/special-live-10/" \
--o lista_final.m3u
+# Lista de URLs
+urls = [
+    "https://www.ms.now/live",
+    "https://globoplay.globo.com/v/4613774/",
+    "https://globoplay.globo.com/ao-vivo/7689934/",
+    "https://globoplay.globo.com/ao-vivo/7690141/",
+    "https://globoplay.globo.com/ao-vivo/7813173/",
+    "https://g1.globo.com/go/goias/ao-vivo/policia-civil-realiza-coletiva-de-imprensa-sobre-o-caso-da-corretora-de-imoveis-encontrada-morta.ghtml",
+    "https://g1.globo.com/sp/campinas-regiao/ao-vivo/eptv-1-campinas-ao-vivo.ghtml",
+    "https://globoplay.globo.com/ao-vivo/14164032",
+    "https://globoplay.globo.com/ao-vivo/2134039/",
+    "https://globoplay.globo.com/v/12749215/",
+    "https://g1.globo.com/rr/roraima/video/ao-vivo-assista-o-jornal-de-roraima-1a-edicao-2923545-1739458038240.ghtml",
+    "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/bom-dia-cidade-ribeirao-preto.ghtml",
+    "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/eptv1.ghtml",
+    "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/eptv-2-ribeirao-e-franca-ao-vivo.ghtml",
+    "https://g1.globo.com/pe/petrolina-regiao/ao-vivo/ao-vivo-assista-ao-gr2.ghtml",
+    "https://g1.globo.com/ap/ao-vivo/assista-ao-bdap-desta-sexta-feira-7.ghtml",
+    "https://abcnews.com/live/video/special-live-10/"
+]
+
+# Monta o comando
+command = ["allfinder"] + urls + ["-o", "lista_final.m3u"]
+
+# Executa
+subprocess.run(command, check=True)
+
+print("Lista gerada com sucesso!")
