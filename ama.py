57a82fed1d689d710ff7eb9a95d7ab01c6bc36a7
punkrastafari <XANDEWERNECK_@HOTMAIL.COM>
2026-02-21 09:34:58 -0300

    Update ama.py

--- ama.py
+++ ama.py
@@ -2,40 +2,9 @@ import subprocess
 
 # Lista de URLs
 urls = [
-    "https://televisao.tv/rtp1",
-    "https://televisao.tv/rtp2",
-    "https://televisao.tv/rtp3",
-    "https://www.bloomberg.com/live/us-btv",
-    "https://www.ms.now/live",
-    "https://www.foxnews.com/video/5614626175001",
-    "https://abcnews.com/live/video/special-live-10/",
-    "https://www.soultv.com.br/channel/167",
-    "https://www.soultv.com.br/channel/176",
-    "https://www.soultv.com.br/channel/178",
-    "https://www.soultv.com.br/channel/214",
-    "https://www.soultv.com.br/channel/332",
-    "https://www.soultv.com.br/channel/78",
-    "https://www.soultv.com.br/channel/73",
-    "https://www.soultv.com.br/channel/166",
-    "https://www.soultv.com.br/channel/140",
-    "https://www.soultv.com.br/channel/137",
-    "https://www.soultv.com.br/channel/230",
-    "https://www.soultv.com.br/channel/181",
-    "https://globoplay.globo.com/v/4613774/",
-    "https://globoplay.globo.com/ao-vivo/7689934/",
-    "https://globoplay.globo.com/ao-vivo/7690141/",
-    "https://globoplay.globo.com/ao-vivo/7813173/",
-    "https://g1.globo.com/go/goias/ao-vivo/policia-civil-realiza-coletiva-de-imprensa-sobre-o-caso-da-corretora-de-imoveis-encontrada-morta.ghtml",
-    "https://g1.globo.com/sp/campinas-regiao/ao-vivo/eptv-1-campinas-ao-vivo.ghtml",
-    "https://globoplay.globo.com/ao-vivo/14164032",
-    "https://globoplay.globo.com/ao-vivo/2134039/",
-    "https://globoplay.globo.com/v/12749215/",
-    "https://g1.globo.com/rr/roraima/video/ao-vivo-assista-o-jornal-de-roraima-1a-edicao-2923545-1739458038240.ghtml",
-    "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/bom-dia-cidade-ribeirao-preto.ghtml",
-    "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/eptv1.ghtml",
-    "https://g1.globo.com/sp/ribeirao-preto-franca/ao-vivo/eptv-2-ribeirao-e-franca-ao-vivo.ghtml",
-    "https://g1.globo.com/pe/petrolina-regiao/ao-vivo/ao-vivo-assista-ao-gr2.ghtml",
-    "https://g1.globo.com/ap/ao-vivo/assista-ao-bdap-desta-sexta-feira-7.ghtml"
+"https://g1.globo.com/sp/campinas-regiao/videos-jornal-da-eptv-1-edicao-campinas-e-piracicaba/video/assista-a-integra-do-eptv-1-campinaspiracicaba-desta-sexta-20-de-fevereiro-de-2026-14365619.ghtml",
+"https://g1.globo.com/df/distrito-federal/df1/video/df1-edicao-de-20022026-14364837.ghtml",
+"https://globoplay.globo.com/v/14354933/?s=0s"
 ]
 
 # Monta o comando
