f1c7d466950805c2f3b8c29bbd8d2122d8655d5d
punkrastafari <XANDEWERNECK_@HOTMAIL.COM>
2026-02-21 09:41:28 -0300

    Update ama.py

--- ama.py
+++ ama.py
@@ -2,9 +2,7 @@ import subprocess
 
 # Lista de URLs
 urls = [
-"https://g1.globo.com/sp/campinas-regiao/videos-jornal-da-eptv-1-edicao-campinas-e-piracicaba/video/assista-a-integra-do-eptv-1-campinaspiracicaba-desta-sexta-20-de-fevereiro-de-2026-14365619.ghtml",
-"https://g1.globo.com/df/distrito-federal/df1/video/df1-edicao-de-20022026-14364837.ghtml",
-"https://globoplay.globo.com/v/14354933/?s=0s"
+"https://www.foxnews.com/video/5614615980001"
 ]
 
 # Monta o comando
