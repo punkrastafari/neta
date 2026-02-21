507194bd5d71fb073a6e54459f76553cd52f0701
punkrastafari <XANDEWERNECK_@HOTMAIL.COM>
2026-02-21 09:43:10 -0300

    Update ama.py

--- ama.py
+++ ama.py
@@ -12,3 +12,33 @@ command = ["allfinder"] + urls + ["-o", "lista_final.m3u"]
 subprocess.run(command, check=True)
 
 print("Lista gerada com sucesso!")
+
+import subprocess
+
+# Lista de URLs
+urls = [
+"https://www.foxnews.com/video/5614615980001"
+]
+
+# Monta o comando
+command = ["allfinder"] + urls + ["-o", "FXN.m3u8"]
+
+# Executa
+subprocess.run(command, check=True)
+
+print("Lista gerada com sucesso!")
+
+import subprocess
+
+# Lista de URLs
+urls = [
+"https://www.foxnews.com/video/5614626175001"
+]
+
+# Monta o comando
+command = ["allfinder"] + urls + ["-o", "FXNB.m3u8"]
+
+# Executa
+subprocess.run(command, check=True)
+
+print("Lista gerada com sucesso!")
